5f66c476d9d7504a2df1aa6e67693bfba0000831
ManuelDev <manueldeveloper17@gmail.com>
2022-04-22 15:28:18 -0500

    Se realizo el diseño del componente OlvidePassword

--- src/pages/OlvidePassword.jsx
+++ src/pages/OlvidePassword.jsx
@@ -1,8 +1,52 @@
+import { Link } from "react-router-dom";
+
 const OlvidePassword = () => {
   return (
-    <div>
-      <h1>OlvidePassword</h1>
-    </div>
+    <>
+      <h1 className="text-sky-600 font-black text-5xl md:text-6xl capitalize">
+        Recupera tu acceso y no pierdas tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        {/* Email */}
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="w-full mt-2 p-3 border rounded-xl bg-gray-50  focus:outline-sky-700"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Enviar Instrucciones"
+          className="w-full bg-sky-700 py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          to="/"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿Ya tienes una cuenta? Inicia sesión
+        </Link>
+        <Link
+          to="/registrar"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          ¿No tienes una cuenta? Regístrate
+        </Link>
+      </nav>
+    </>
   );
 };
 
